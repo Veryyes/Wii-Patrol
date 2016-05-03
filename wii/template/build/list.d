da85ea4dd18f33bec7660c94c5f3f77664a5f3f1
Brandon <veryyes10@yahoo.com>
2016-05-03 14:29:45 -0400

    finished

--- wii/template/build/list.d
+++
@@ -1,5 +0,0 @@
-list.o: \
- /home/brandon/Programming/devkitpro/Wii-Patrol/wii/template/source/list.c \
- /home/brandon/Programming/devkitpro/Wii-Patrol/wii/template/include/list.h
-
-/home/brandon/Programming/devkitpro/Wii-Patrol/wii/template/include/list.h:
